e8c1f11ea108a2a09c2bfe3ac1a192a63a14a09b
videetparekh <videetparekh0047@gmail.com>
2019-08-14 12:25:37 -0400

    Updated Combination Sum

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,26 +1,21 @@
 class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-        int sum = 0;
         List<List<Integer>> values = new ArrayList<>();
-        combinationSum(values, new ArrayList<Integer>(), candidates, target, sum);
+        combinationSum(values, new ArrayList<Integer>(), candidates, target, 0);
         return values;
     }
     
-    private void combinationSum(List<List<Integer>> values, List<Integer> combination, int[] candidates, int target, int currentSum) {
-        for (int i=0;i<candidates.length; i++) {
-            if (candidates[i] + currentSum == target) {
-                List<Integer> newCombo = new ArrayList<>(combination);
-                newCombo.add(candidates[i]);
-                Collections.sort(newCombo);
-                if(!values.contains(newCombo)) values.add(newCombo);
-            }
-            else if (candidates[i] < target - currentSum) {
-                List<Integer> newCombo = new ArrayList<>(combination);
-                newCombo.add(candidates[i]);
-                combinationSum(values, newCombo, candidates, target, currentSum+candidates[i]);
-            }
-        }
-        return;
+    private void combinationSum(List<List<Integer>> values, List<Integer> combination, int[] candidates, int target, int start) {
+        if(target > 0){
+    		for(int i = start; i < candidates.length && target >= candidates[i]; i++){
+    			combination.add(candidates[i]);
+    			combinationSum(values, combination, candidates, target - candidates[i], i);
+    			combination.remove(combination.size() - 1);
+    		}
+    	}
+    	else if(target == 0 ){
+    		values.add(new ArrayList<Integer>(combination));
+    	}
     }
 }
